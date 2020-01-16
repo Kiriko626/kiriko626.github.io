2610f99529b51b5b38b35f3b36b109fb95d4237f
Kiriko626 <kiriko626@gmail.com>
2020-01-16 16:12:35 +0500

    init commit

---
+++ precache-manifest.1695b2c87d0d107641e6c9fff9e94cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36277dd8eafbfe7af20961b6d83b2e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ee888b046dd649567e7",
+    "url": "/static/css/main.951ec6ad.chunk.css"
+  },
+  {
+    "revision": "42a7a7fce0b95e5307cb",
+    "url": "/static/js/2.ebce95d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ebce95d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ee888b046dd649567e7",
+    "url": "/static/js/main.52065595.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
